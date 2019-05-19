4b58703b389ef20b3cd95ccd4436651c982a5e0f
Pradeep Kumar Singh <raj.ratedr001@gmail.com>
2019-05-19 20:14:59 +0530

    artifactory

--- src/com/devops/CDPipeline.groovy
+++ src/com/devops/CDPipeline.groovy
@@ -9,7 +9,7 @@ def execute() {
    //rtGradle.resolver server: server, repo: 'libs-release'
    rtGradle.deployer server: server, repo: 'example-repo-local'
    rtGradle.tool = 'gradle'
-   def buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'clean artifactoryPublish'
+   def buildInfo = rtGradle.run buildFile: 'build.gradle', tasks: 'clean build artifactoryPublish'
    //rootDir: "projectDir/",
    println "buildInfo: " + buildInfo
 }
