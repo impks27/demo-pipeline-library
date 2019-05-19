ca9934ae6688addb5f22e95c291e80ed57c04c94
Pradeep Kumar Singh <raj.ratedr001@gmail.com>
2019-05-19 16:47:23 +0530

    artifactory

--- src/com/devops/ArtifactoryGradleJob.groovy
+++ src/com/devops/ArtifactoryGradleJob.groovy
@@ -1,12 +1,14 @@
 package com.devops
 
-def init() {
-  println "Executing ArtifactoryGradleJob!"
-  def server = Artifactory.server 'artifactory-instance'
-  def rtGradle = Artifactory.newGradleBuild()
-  //rtGradle.resolver server: server, repo: 'libs-release'
-  rtGradle.deployer server: server, repo: 'example-repo-local'
-  rtGradle.tool = 'gradle'
-  def buildInfo = rtGradle.run rootDir: "projectDir/", buildFile: 'build.gradle', tasks: 'clean artifactoryPublish'
-  println "buildInfo: " + buildInfo
+class ArtifactoryGradleJob {
+  def init() {
+    println "Executing ArtifactoryGradleJob!"
+    def server = Artifactory.server 'artifactory-instance'
+    def rtGradle = Artifactory.newGradleBuild()
+    //rtGradle.resolver server: server, repo: 'libs-release'
+    rtGradle.deployer server: server, repo: 'example-repo-local'
+    rtGradle.tool = 'gradle'
+    def buildInfo = rtGradle.run rootDir: "projectDir/", buildFile: 'build.gradle', tasks: 'clean artifactoryPublish'
+    println "buildInfo: " + buildInfo
+  }
 }
