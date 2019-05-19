9ee7cafda2ad039917267ffb4f9d3dc0293faa34
Pradeep Kumar Singh <raj.ratedr001@gmail.com>
2019-05-19 16:38:35 +0530

    artifactory

--- src/com/devops/ArtifactoryGradleJob.groovy
+++ src/com/devops/ArtifactoryGradleJob.groovy
@@ -2,7 +2,7 @@ package com.devops
 
 def init() {
   println "Executing ArtifactoryGradleJob!"
-  def server = Artifactory.server('artifactory-instance')
+  def server = Artifactory.server 'artifactory-instance'
   def rtGradle = Artifactory.newGradleBuild()
   //rtGradle.resolver server: server, repo: 'libs-release'
   rtGradle.deployer server: server, repo: 'example-repo-local'
